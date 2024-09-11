e8927ede3ed380fb88441a064f60995ecd6ce2ed
Joe Heck <heckj@mac.com>
2024-09-11 13:17:35 -0700

    abstracts for docs

--- Sources/Heightmap/Heightmap.swift
+++ Sources/Heightmap/Heightmap.swift
@@ -1,19 +1,70 @@
+/// A two-dimensional array of unit-values, used to represent a height map.
 public struct Heightmap: Sendable, Hashable {
+    /// The linear interior contents of the two-dimensional array.
+    ///
+    /// To compute an index location, use ``XZIndex/XZtoStride(x:z:width:)`` or access
+    /// the contents through ``subscript(_:)-8ag3d`` or ``subscript(_:_:)``.
     public var contents: [Float]
+    /// The width of the array.
+    ///
+    /// This typically aligns to the ``XZIndex/x`` coordinate in ``XZIndex``.
     public let width: Int
+    /// The height of the array.
+    ///
+    /// This typically aligns to the ``XZIndex/z`` coordinate in ``XZIndex``.
     public let height: Int
 
+    /// Creates a new height map from the collection of floating point values that you provide.
+    /// - Parameters:
+    ///   - contents: The array of floating point values that represents the heightmap.
+    ///   - width: The width of the height map
+    ///
+    /// Values that extend beyond the length of a two-dimensional array are truncated.
     public init(_ contents: [Float], width: Int) {
         self.width = width
+        precondition(contents.count >= width)
         height = contents.count / width
         // make a copy of the array, truncating any extra stuff...
         self.contents = Array(contents[0 ..< (height * width)])
     }
 
+    /// Creates a new height map from the collection of floating point values that you provide.
+    /// - Parameters:
+    ///   - contents: The array of floating point values that represents the heightmap.
+    ///   - width: The width of the height map
+    ///   - depth: The depth of the height map
+    ///
+    /// Values that extend beyond the length of a two-dimensional array are truncated.
+    public init(_ contents: [Float], width: Int, depth: Int) {
+        self.width = width
+        height = depth
+        // make a copy of the array, truncating any extra stuff...
+        precondition(contents.count >= width * height)
+        self.contents = Array(contents[0 ..< (height * width)])
+    }
+
+    /// Creates a new height map normalizing the collection of floating point values that you provide to unit values.
+    /// - Parameters:
+    ///   - contents: The array of floating point values that represents the heightmap.
+    ///   - width: The width of the height map
+    ///
+    /// The normalization process only changes the values if the collection of floating point values
+    /// provided extends outside of the values 0.0 to 1.0.
+    /// Values that extend beyond the length of a two-dimensional array are truncated.
+    public init(normalizing: [Float], width: Int) {
+        self.width = width
+        height = normalizing.count / width
+        // make a copy of the array, truncating any extra stuff...
+        let temp = Array(normalizing[0 ..< (height * width)])
+        contents = temp.normalizeUnitValues()
+    }
+
+    /// Returns the floating point value at the width and depth you provide.
     public subscript(_ x: Int, _ z: Int) -> Float {
         contents[XZIndex.XZtoStride(x: x, z: z, width: width)]
     }
 
+    /// Returns the floating point value at the width and depth you provide.
     public subscript(_ xz: XZIndex) -> Float {
         contents[XZIndex.XZtoStride(x: xz.x, z: xz.z, width: width)]
     }
--- Sources/Heightmap/XZIndex.swift
+++ Sources/Heightmap/XZIndex.swift
@@ -1,12 +1,24 @@
+/// A two-dimensional coordinate index to a height map.
 public struct XZIndex: Sendable, Hashable {
+    /// The width coordinate of the index location.
     public let x: Int
+    /// The depth coordinate of the index location.
     public let z: Int
 
+    /// Creates a two-dimensional coordinate index to a height map.
+    /// - Parameters:
+    ///   - x: <#x description#>
+    ///   - z: <#z description#>
     public init(x: Int, z: Int) {
         self.x = x
         self.z = z
     }
 
+    /// Converts a linear index position to a 2D integer coordinate.
+    /// - Parameters:
+    ///   - stride: The linear index location.
+    ///   - width: The width of the height map.
+    /// - Returns: The XZIndex that represents the location you provided.
     @inlinable
     public static func strideToXZ(_ stride: Int, width: Int) -> XZIndex {
         var z = 0
@@ -17,6 +29,12 @@ public struct XZIndex: Sendable, Hashable {
         return XZIndex(x: remaining, z: z)
     }
 
+    /// Converts an 2D integer coordinate to a linear index position.
+    /// - Parameters:
+    ///   - x: The width coordinate.
+    ///   - z: The depth coordinate.
+    ///   - width: The width of the height map.
+    /// - Returns: The integer location in a flattened array.
     @inlinable
     public static func XZtoStride(x: Int, z: Int, width: Int) -> Int {
         let minorOffset = z * width
