f887f44f2679a69d38119f4b91a5f6c2dbee78d3
Joe Heck <heckj@mac.com>
2024-09-11 12:24:23 -0700

    move static func to internal extension on Array of Floats

--- Sources/Heightmap/Heightmap+normalize.swift
+++ Sources/Heightmap/Heightmap+normalize.swift
@@ -1,31 +1,11 @@
 public extension Heightmap {
-    static func normalize(_ contents: [Float], expand: Bool = false) -> [Float] {
-        if contents.isEmpty { return contents }
-        var minValue: Float = contents[0]
-        var maxValue: Float = minValue
-        for floatValue in contents {
-            minValue = Swift.min(minValue, floatValue)
-            maxValue = Swift.max(maxValue, floatValue)
-        }
-        if minValue >= 0, maxValue <= 1.0, !expand {
-            return contents
-        }
-        var normalizedData: [Float] = []
-        let range: Float = maxValue - minValue
-        let offset: Float = 0.0 - minValue
-        for floatValue in contents {
-            normalizedData.append((floatValue + offset) / range)
-        }
-        return normalizedData
-    }
-
     func normalize(expand: Bool = false) -> Self {
-        let normalizedData = Self.normalize(contents, expand: expand)
+        let normalizedData = contents.normalizeUnitValues(expand: expand)
         return Heightmap(normalizedData, width: width)
     }
 
     mutating func normalized(expand: Bool = false) {
-        contents = Self.normalize(contents, expand: expand)
+        contents = contents.normalizeUnitValues(expand: expand)
     }
 
     init(bytes: [UInt8], width: Int) {
@@ -35,6 +15,6 @@ public extension Heightmap {
         let temp = bytes[0 ..< (height * width)].map { byte in
             Float(byte)
         }
-        contents = Self.normalize(temp)
+        contents = temp.normalizeUnitValues()
     }
 }
